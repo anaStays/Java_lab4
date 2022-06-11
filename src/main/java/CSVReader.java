f73eabad78df75ed3a3422f1a6ce4cb212aa2f7d
Nastya <an_vorotilina@mail.ru>
2022-06-11 07:52:20 +0300

    Division using in reader

--- src/main/java/CSVReader.java
+++ src/main/java/CSVReader.java
@@ -1,2 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CSVReader {
+    Map<String,Division> map = new HashMap<>();
+    private Long idSequence=0L;
+    /**
+     * @param name Имя подразделения
+     * @return Объект подразделения
+     */
+    private Division getDivision(String name){
+        if(map.containsKey(name)){
+            return map.get(name);
+        }
+        else{
+            Division division = new Division(++idSequence,name);
+            map.put(name, division);
+            return division;
+        }
+    }
 }
