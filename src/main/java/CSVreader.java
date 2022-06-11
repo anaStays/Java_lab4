408a64041a8572a288c996c4b4f2a98ff133857d
Nastya <an_vorotilina@mail.ru>
2022-06-11 08:27:18 +0300

    added example in main

--- src/main/java/CSVreader.java
+++ src/main/java/CSVreader.java
@@ -1,12 +1,19 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CSVReader {
+public class CSVreader {
     Map<String,Division> map = new HashMap<>();
     private Long idSequence=0L;
     /**
-     * @param name Имя подразделения
-     * @return Объект подразделения
+     * @param name Division name
+     * @return Division instance
      */
     private Division getDivision(String name){
         if(map.containsKey(name)){
@@ -17,5 +24,36 @@ public class CSVReader {
             map.put(name, division);
             return division;
         }
+    }
+
+    /**
+     * @param path Path to file
+     * @return staff data
+     * @throws IOException
+     */
+    public ArrayList<Person> download(String path) throws IOException {
+        ArrayList<Person> persons = new ArrayList<>();
+        au.com.bytecode.opencsv.CSVReader reader;
+        try {
+            reader = new au.com.bytecode.opencsv.CSVReader(new FileReader(path),';','"',1);
+        }
+        catch (FileNotFoundException e){
+            throw new FileNotFoundException(path);
+        }
+
+        String[] nextLine;
+        while ((nextLine = reader.readNext()) != null){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            Person person = new Person(Long.valueOf(nextLine[0]),
+                    nextLine[1],
+                    nextLine[2],
+                    LocalDate.parse(nextLine[3],formatter),
+                    getDivision(nextLine[4]),
+                    Integer.parseInt(nextLine[5])
+            );
+
+            persons.add(person);
+        }
+        return persons;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
+
+import java.io.IOException;
+import java.lang.String;
+import java.util.List;
+
 public class Main {
+    public  static void main(String args[]) throws IOException {
+        CSVreader reader = new CSVreader();
+        List<Person> peopleList = reader.download("src/main/resources/foreign_names.csv");
+
+        for (Person person: peopleList) {
+            System.out.println(person.toString());
+        }
+    }
 }
